1766b14cccfa513c4fbe425192caa75c571f8c43
Lê Văn Thiện <52475801+epitchi@users.noreply.github.com>
2021-07-20 15:12:42 +0700

    Sent message

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const wss = new WebSocket.Server({
 wss.on("connection", ws =>{
     console.log("New client connected!");
 
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`);
+    });
+
     ws.on("close", () => {
         console.log("Client has disconnected!");
     })
